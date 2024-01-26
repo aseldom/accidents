eee5543f3f2820b718ec3a86ae1e1291a6a0b718
aseldom <a.seldom@gmail.com>
2024-01-26 08:42:39 +0600

    4. Form с композиционным объектом [#305522 #476049]

--- src/main/java/ru/job4j/accidents/service/SimpleAccidentService.java
+++ src/main/java/ru/job4j/accidents/service/SimpleAccidentService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.AccidentType;
 import ru.job4j.accidents.model.Rule;
-import ru.job4j.accidents.repository.MemoryAccidentRepository;
+import ru.job4j.accidents.repository.AccidentRepository;
 
 import java.util.Collection;
 import java.util.Optional;
@@ -16,34 +16,38 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class SimpleAccidentService implements AccidentService {
 
-    private final MemoryAccidentRepository memoryAccidentRepository;
+    private final AccidentRepository accidentRepository;
     private final SimpleRuleService simpleRuleService;
     private final SimpleAccidentTypeService simpleAccidentTypeService;
 
     @Override
     public Accident add(Accident accident) {
-        return memoryAccidentRepository.add(accident);
+        return accidentRepository.add(accident);
     }
 
     @Override
     public boolean update(Accident accident, Set<Integer> rIds) {
-        Set<Rule> rules = rIds.stream()
-                .map(x -> simpleRuleService.findById(x).get())
-                .collect(Collectors.toSet());
-        Optional<AccidentType> accidentTypeOptional = simpleAccidentTypeService.findById(accident.getType().getId());
-        accident.setType(accidentTypeOptional.get());
-        accident.setRules(rules);
-        return accident.getId() == -1 ? add(accident) != null : memoryAccidentRepository.update(accident);
+        try {
+            Set<Rule> rules = rIds.stream()
+                    .map(x -> simpleRuleService.findById(x).get())
+                    .collect(Collectors.toSet());
+            Optional<AccidentType> accidentTypeOptional = simpleAccidentTypeService.findById(accident.getType().getId());
+            accident.setType(accidentTypeOptional.get());
+            accident.setRules(rules);
+            return accidentRepository.update(accident) || add(accident) != null;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
     public Optional<Accident> findById(int id) {
-        return memoryAccidentRepository.findById(id);
+        return accidentRepository.findById(id);
     }
 
     @Override
     public Collection<Accident> findAll() {
-        return memoryAccidentRepository.findAll();
+        return accidentRepository.findAll();
     }
 
 }
