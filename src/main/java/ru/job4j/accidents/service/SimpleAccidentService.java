7a931c317cfc8b88bc9588f597c4f67684b771e3
aseldom <a.seldom@gmail.com>
2024-01-27 08:23:02 +0600

    4. Form с композиционным объектом [#305522 #476049]

--- src/main/java/ru/job4j/accidents/service/SimpleAccidentService.java
+++ src/main/java/ru/job4j/accidents/service/SimpleAccidentService.java
@@ -3,14 +3,12 @@ package ru.job4j.accidents.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.job4j.accidents.model.Accident;
-import ru.job4j.accidents.model.AccidentType;
-import ru.job4j.accidents.model.Rule;
 import ru.job4j.accidents.repository.AccidentRepository;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -27,17 +25,10 @@ public class SimpleAccidentService implements AccidentService {
 
     @Override
     public boolean update(Accident accident, Set<Integer> rIds) {
-        try {
-            Set<Rule> rules = rIds.stream()
-                    .map(x -> simpleRuleService.findById(x).get())
-                    .collect(Collectors.toSet());
-            Optional<AccidentType> accidentTypeOptional = simpleAccidentTypeService.findById(accident.getType().getId());
-            accident.setType(accidentTypeOptional.get());
-            accident.setRules(rules);
-            return accidentRepository.update(accident) || add(accident) != null;
-        } catch (Exception e) {
-            return false;
-        }
+        accident.setType(simpleAccidentTypeService.findById(accident.getType().getId()).get());
+        accident.setRules(new HashSet<>(simpleRuleService.findAllByIds(rIds))
+        );
+        return accidentRepository.update(accident) || add(accident) != null;
     }
 
     @Override
